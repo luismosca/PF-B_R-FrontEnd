3be09d77ac6242e83e091e2cb46bf5e67e28667a
Harold David Gonzalez Usuga <hadavidg@gmail.com>
2023-09-22 16:06:12 -0500

    formualrios editados, detail

--- src/Components/CardsContainer/CardsContainer.jsx
+++ src/Components/CardsContainer/CardsContainer.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import style from "./CardsContainer.module.css";
 
 const CardContainer = ({ id, name, description, image, date, location, age }) => {
+  const img = image ? image : "https://this-person-does-not-exist.com/img/avatar-gen4a0cc4d9a2581818a703a23e62600295.jpg"
   return (
     <div className={style.contenedor}>
       <Link to={`/detail/${id}`} className={style.link}>
         <h2>{name}</h2>
       </Link>
       <Link to={`/detail/${id}`}>
-        <img src={"https://this-person-does-not-exist.com/img/avatar-gen4a0cc4d9a2581818a703a23e62600295.jpg"} alt="" className={style.image}/>
+        <img src={img} alt="" className={style.image}/>
       </Link>
       <Link to={`/detail/${id}`} className={style.link}>
         <h2>Date: {date}</h2>
