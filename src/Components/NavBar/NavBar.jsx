9e9b9fe39b9d11cd3446469ed81e68d10c1dc0ad
Piero <piero7210@hotmail.com>
2023-10-01 21:07:09 -0500

    Nueva carpeta auth-helpers y añadido funciones token a login y navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { Link, } from 'react-router-dom';
-import style from './NavBar.module.css'; 
-
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./NavBar.module.css";
+import { deleteToken } from "../../auth-helpers/auth-helpers";
 
 const NavBar = () => {
+  const handleLogOut = () => {
+    deleteToken();
+  }
+
   return (
     <>
-    <nav className={style.navBar}>
-      <ul>
-        <li>
-          <Link to="/profile">Perfil</Link>
-        </li>
-        <li>
-          <Link to="/ByR">Acerca de B&R</Link>
-        </li>
-        <li>
-          <Link to="/donacion">¡Únete a la causa!</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav className={style.navBar}>
+        <ul>
+          <li>
+            <Link to="/profile">Perfil</Link>
+          </li>
+          <li>
+            <Link to="/ByR">Acerca de B&R</Link>
+          </li>
+          <li>
+            <Link to="/donacion">¡Únete a la causa!</Link>
+          </li>
+          <li>
+            <Link to="/home" onClick={handleLogOut}>Cerrar Sesión</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
